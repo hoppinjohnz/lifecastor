eebfe7c799e3c70da24a72e5cfa8d1d0e254eb47
hoppinjohnz <johnzhu00@gmail.com>
2012-08-08 11:14:06 -0400

    rails new lifecator --skil-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifecastor::Application.config.secret_token = 'b6797827044ca8fe46dcebdeabbc73684653f3d71fc2936802651695812d3920ca26e96931b0ea81908c5cf1e4d0b479fbcbc9b29e9466aa113ea9e83b856dcf'

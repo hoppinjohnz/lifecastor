22e1390c297737552d86b59fb95a31eb20880c81
hoppinjohnz <johnzhu00@gmail.com>
2012-08-09 00:29:52 -0400

    css; to 5.3.4 pretty rspec

--- README.md
+++ README.md
@@ -14,8 +14,3 @@ This is an application for [*Life-Time Personal Finance Forecast/Simulation*](ht
 
 Please send comments and report issues to John at john@zolutionsllc.com.
 
-<!---
-This is the sample application for
-[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
-by [Michael Hartl](http://michaelhartl.com/).
--->
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,22 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Lifecastor'" do
-      visit '/static_pages/home'
-      page.should have_content('Welcome to Lifecastor!')
+      visit home_path
+      page.should have_content('Welcome to Lifecastor')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', text: "Lifecastor | Contact")
     end
   end
 end

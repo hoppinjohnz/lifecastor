133043d396d58d4a8cec1b45eaa6a170078b5d6c
hoppinjohnz <johnzhu00@gmail.com>
2012-08-10 13:01:06 -0400

    add mixed case tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,6 +62,7 @@ describe User do
       end      
     end
   end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -71,7 +72,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email address is already taken" do
+  describe "when email address is already taken in uppercase" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -79,6 +80,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
